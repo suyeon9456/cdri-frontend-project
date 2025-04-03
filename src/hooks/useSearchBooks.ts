16e93425f7942562f49b8009bbe8c0f20f2fac92
suyeon9456 <suyeon9456@naver.com>
2025-04-03 22:50:19 +0900

    type: type 수정

--- src/hooks/useSearchBooks.ts
+++ src/hooks/useSearchBooks.ts
@@ -3,12 +3,16 @@ import { RequestGetBooks, ResponseGetBooks } from '../types/getBooks';
 import { searchBooksQueryKey } from '../utils/queryKeys';
 import { getSearchBooks } from '../remotes';
 
-const useSearchBooks = ({ query }: { query: RequestGetBooks }) => {
+const useSearchBooks = ({
+  query,
+}: {
+  query: { [K in keyof RequestGetBooks]: string | number };
+}) => {
   const { data: books } = useQuery(getSearchBooksQueryKeys(query));
   return { books };
 };
 
-const getSearchBooksQueryKeys = (query: RequestGetBooks) =>
+const getSearchBooksQueryKeys = (query: { [K in keyof RequestGetBooks]: string | number }) =>
   queryOptions<ResponseGetBooks>({
     queryKey: searchBooksQueryKey.search(query),
     queryFn: () => getSearchBooks(query),
--- src/remotes.ts
+++ src/remotes.ts
@@ -1,11 +1,13 @@
 import axios from 'axios';
-import { ResponseGetBooks } from './types/getBooks';
+import { RequestGetBooks, ResponseGetBooks } from './types/getBooks';
 
 const REST_API_KEY = import.meta.env.VITE_KAKAO_REST_API_KEY;
 
-export const getSearchBooks = async (): Promise<ResponseGetBooks> => {
+export const getSearchBooks = async (query: {
+  [K in keyof RequestGetBooks]: string | number;
+}): Promise<ResponseGetBooks> => {
   const { data } = await axios.get('https://dapi.kakao.com/v3/search/book?target=title', {
-    params: { query: '미움받을 용기', page: 1 },
+    params: query,
     headers: {
       Authorization: `KakaoAK ${REST_API_KEY}`,
     },
